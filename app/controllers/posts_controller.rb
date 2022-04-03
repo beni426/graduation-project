3380694bb87717c31e06ad0f93beff58e75358ac
Ryu <goskn88006@yahoo.co.jp>
2022-04-03 18:13:11 +0900

    blog 投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all 
+
   end
 
   # GET /posts/1 or /posts/1.json
   def show
+    @posts = Post.where(user_id: @post.user.id).order(created_at: :desc)
+    @comments = @post.comments
+    @comment = @post.comments.build
   end
 
   # GET /posts/new
@@ -21,19 +25,25 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
+    @post = current_user.posts.build(post_params)
+    if params[:back]
+      render :new
+    else 
+   respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to posts_url, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-    end
+     end
+   end
   end
-
+  def confirm
+    @post = current_user.posts.build(post_params)
+    render :new if @post.invalid?
+  end
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
@@ -65,6 +75,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :image)
+      params.require(:post).permit(:title, :description, :image, :image_cache)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    has_many :comments,dependent: :destroy
+    validates :image, presence: false
+    mount_uploader :image, ImageUploader
 end
