0bab94cb7f9d8091110574ed9e29cb806ae0142d
Ryu <goskn88006@yahoo.co.jp>
2022-04-11 13:57:53 +0900

    headerとfooterをパーシャルでレンダーする

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fill: [160,250]
+  process resize_to_fill: [130,200]
   storage :file
  
   def store_dir
