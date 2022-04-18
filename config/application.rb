34369551908d6c1ef8ff51e50c57328eca2372f8
Ryu <goskn88006@yahoo.co.jp>
2022-04-18 21:30:21 +0900

    heroku デプロイで画像が表示されない

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 Bundler.require(*Rails.groups(:assets => %w(development test)))
-config.assets.compile = true
-config.serve_static_assets= true
+
 
 module Kiraibar
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
+    config.assets.compile = true
+    config.serve_static_assets= true
     config.i18n.available_locales = %i(ja en)
     config.i18n.enforce_available_locales = true
     config.active_record.default_timezone = :local
